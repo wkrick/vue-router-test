8507a9eabb14aba1e6411548356d2d142e03d05f
William Krick <wkrick@gmail.com>
2025-04-13 14:17:17 -0400

    renamed component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import InventoryGUI from '@/components/InventoryUI.vue'
+import InventoryUI from '@/components/InventoryUI.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/:state?',
-      component: InventoryGUI,
+      component: InventoryUI,
     },
   ],
 })
